894f505f97ce5888cf3a53e6a585a5aa4710399a
Vinicius Rossado <vinirossado@gmail.com>
2024-02-22 21:31:01 +0200

    First Parallel exercise all threads at same time

---
+++ ParallelProgramming/ParallelProgramming/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//There are a lot mix . ? and - because all 3 threads are executing at the same time 
+static void Write(char c)
+{
+    var i = 1000;
+    while (i-- > 0)
+    {
+        Console.Write(c);
+    }
+}
+
+
+Task.Factory.StartNew(() => Write('.'));
+
+var t = new Task(() => Write('?'));
+t.Start();
+
+Write('-');
+Console.WriteLine("Main program done!");
+Console.ReadKey();
